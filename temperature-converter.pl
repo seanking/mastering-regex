5d98a4f3ab196d9ce6cb4c81d68e61c982655238
Sean King <r.sean.king@gmail.com>
2016-12-03 18:02:00 -0500

    Enable  decimals, negative, and positive values and

--- temperature-converter.pl
+++ temperature-converter.pl
@@ -2,7 +2,7 @@ print "Enter a temperature in Celsius:\n";
 $celsius = <STDIN>;
 chomp($celsius);
 
-if ($celsius =~ m/^[0-9]+$/) {
+if ($celsius =~ m/^[-+]?[0-9]+([\.0-9]*)?$/) {
     $fahrenhiet = ($celsius * 9/5) + 32;
     printf "%.2f C is %.2f F.\n", $celsius, $fahrenhiet;
 } else {
