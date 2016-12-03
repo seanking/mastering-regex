cacce3f3571ef1516a7b826355000f5024e79dd8
Sean King <r.sean.king@gmail.com>
2016-12-03 17:57:54 -0500

    Improve output by formatting temps to 2 decimals

--- temperature-converter.pl
+++ temperature-converter.pl
@@ -4,7 +4,7 @@ chomp($celsius);
 
 if ($celsius =~ m/^[0-9]+$/) {
     $fahrenhiet = ($celsius * 9/5) + 32;
-    print "$celsius C is $fahrenhiet F.\n";
+    printf "%.2f C is %.2f F.\n", $celsius, $fahrenhiet;
 } else {
     print "Expecting a number, so I don't understand \"$celsius\".\n";
 }
