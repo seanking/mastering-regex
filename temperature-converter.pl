eb26d9fe4e77e7c15e147a553b2e7879c9c64430
Sean King <r.sean.king@gmail.com>
2016-12-03 18:18:06 -0500

    Support converting from celsius or fahrenhiet

--- temperature-converter.pl
+++ temperature-converter.pl
@@ -1,10 +1,19 @@
 print "Enter a temperature in Celsius:\n";
-$celsius = <STDIN>;
-chomp($celsius);
+$input = <STDIN>;
+chomp($input);
 
-if ($celsius =~ m/^[-+]?[0-9]+([\.0-9]*)?$/) {
-    $fahrenhiet = ($celsius * 9/5) + 32;
-    printf "%.2f C is %.2f F.\n", $celsius, $fahrenhiet;
+if ($input =~ m/^([-+]?[0-9]+([\.0-9]*)?)([CF])$/) {
+    $temp = $1;
+    $type = $3;
+
+    if ($type eq 'C') {
+        $fahrenhiet = ($temp * 9/5) + 32;
+        printf "%.2f C is %.2f F.\n", $temp, $fahrenhiet;
+    } else {
+        $celsius = ($temp -32) * 5/9;
+        printf "%.2f F is %.2f C.\n", $temp, $celsius;
+    }
+
 } else {
-    print "Expecting a number, so I don't understand \"$celsius\".\n";
+    print "Expecting a number, so I don't understand \"$input\".\n";
 }
